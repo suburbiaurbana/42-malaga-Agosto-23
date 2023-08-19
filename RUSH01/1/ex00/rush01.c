89f7dd57c60ef4502434434a32faeb3792bdcf2c
Rubén Díaz Fraga <89214621+rubendiazzz@users.noreply.github.com>
2023-08-19 19:14:09 +0200

    Update rush01.c

--- RUSH01/1/ex00/rush01.c
+++ RUSH01/1/ex00/rush01.c
@@ -30,7 +30,28 @@ void print_grid() {
 
 // FUNCIÓN PARA APLICAR RESTRICCIONES BÁSICAS SEGÚN LAS PISTAS
 void apply_basic_constraints() {
-    // PUEDES COPIAR LA IMPLEMENTACIÓN DEL FRAGMENTO ANTERIOR AQUÍ
+    int i = 0;
+    while (i < 16) {
+        //Si la pista es 1, coloca un edificio de altura 4en la posición correspondiente.
+        if (clues[i] == 1) {
+            if (i < 4) grid[0][i] = 4; // Pistas de arriba
+            else if (i < 8) grid[i - 4][3] = 4; // Pistas de la derecha
+            else if (i < 12) grid[3][11 - i] = 4; // Pistas de abajo
+            else grid[12 - i][0] = 4; // Pistas de la izquierda
+        }
+        //Si la pista es 4, coloca edificios en orden ascendente.
+        else if (clues[i] == 4) {
+            int j = 0;
+            while (j < 4) {
+                if (i < 4) grid[j][i] = j + 1; // Pistas de arriba
+                else if (i < 8) grid[i - 4][3 - j] = j + 1; // Pistas de la derecha
+                else if (i < 12) grid[3 - j][11 - i] = j + 1; // Pistas de abajo
+                else grid[12 - i][j] = j + 1; // Pistas de la izquierda
+                j++;
+            }
+        }
+        i++;
+    }
 }
 
 int main() {
