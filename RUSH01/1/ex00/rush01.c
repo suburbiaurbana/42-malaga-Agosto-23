56b1792a23b4669cb0f5884bb0c9efd8910354ef
Rubén Díaz Fraga <89214621+rubendiazzz@users.noreply.github.com>
2023-08-19 19:07:11 +0200

    Update rush01.c

--- RUSH01/1/ex00/rush01.c
+++ RUSH01/1/ex00/rush01.c
@@ -1,55 +1,36 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-//TABLA 4X4
+// DEFINIENDO LA CUADRÍCULA (MATRIZ 4X4 INICIALIZADA CON CEROS)
+int grid[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
-int grid[4][4] = 0;
+// PISTAS DE EJEMPLO (ARRIBA, DERECHA, ABAJO, IZQUIERDA - EN SENTIDO HORARIO ALREDEDOR DE LA CUADRÍCULA)
+int clues[16] = {2, 2, 1, 3, 2, 2, 3, 1, 2, 3, 1, 2, 1, 3, 2, 2};
 
-//NUMEROS DE EJEMPLO PARA PROBAR
-
-int nums[16] = {2, 2, 1, 3, 2, 2, 3, 1, 2, 3, 1, 2, 1, 3, 2, 2};
-
-//RESTRICCIONES BASICAS O NORMAS
-
-void apply_basic_constraints() {
-    int i = 0;
-    while (i < 16) {
-        // Si la pista es 1, coloca un edificio de altura 4 en la posicion correcta.
-        if (clues[i] == 1) {
-            if (i < 4) grid[0][i] = 4; // Pistas arriba.
-            else if (i < 8) grid[i - 4][3] = 4; // Pistas derecha.
-            else if (i < 12) grid[3][11 - i] = 4; // Pistas abajo.
-            else grid[12 - i][0] = 4; // Pistas izquierda.
-        }
-        // Si la pista es 4, coloca edificios en orden ascendente.
-        else if (clues[i] == 4) {
-            int j = 0;
-            while (j < 4) {
-                if (i < 4) grid[j][i] = j + 1; // Pistas de arriba.
-                else if (i < 8) grid[i - 4][3 - j] = j + 1; // Pistas de la derecha.
-                else if (i < 12) grid[3 - j][11 - i] = j + 1; // Pistas de abajo.
-                else grid[12 - i][j] = j + 1; // Pistas de la izquierda.
-                j++;
-            }
-        }
-        i++;
-    }
+// FUNCIÓN PARA ESCRIBIR UN SOLO CARÁCTER EN LA SALIDA ESTÁNDAR
+void write_char(char c) {
+    write(1, &c, 1);
 }
 
-//AHORA A IMPRIMIR LA TABLA
-
+// FUNCIÓN PARA IMPRIMIR LA CUADRÍCULA USANDO LA FUNCIÓN WRITE
 void print_grid() {
     int i = 0;
     while (i < 4) {
         int j = 0;
         while (j < 4) {
-            printf("%d ", grid[i][j]);
+            char c = grid[i][j] + '0';
+            write_char(c);
+            write_char(' ');
             j++;
         }
-        printf("\n");
+        write_char('\n');
         i++;
     }
+}
+
+// FUNCIÓN PARA APLICAR RESTRICCIONES BÁSICAS SEGÚN LAS PISTAS
+void apply_basic_constraints() {
+    // PUEDES COPIAR LA IMPLEMENTACIÓN DEL FRAGMENTO ANTERIOR AQUÍ
 }
 
 int main() {
