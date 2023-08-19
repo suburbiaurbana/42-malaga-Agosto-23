d27892f95a5f9b91dedee827f895214fe5bd058e
Ruben Diaz Fraga <rdiaz-fr@c2r7s3.42malaga.com>
2023-08-19 18:17:32 +0200

    segundo commit rush

---
+++ RUSH01/1/ex00/rush01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//TABLA 4X4
+
+int grid[4][4] = 0;
+
+//NUMEROS DE EJEMPLO PARA PROBAR
+
+int nums[16] = {2, 2, 1, 3, 2, 2, 3, 1, 2, 3, 1, 2, 1, 3, 2, 2};
+
+//AHORA A IMPRIMIR LA TABLA
+
+void print_grid()
+{
+	
+}
